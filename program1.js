0bdea5296484e4844b18a771960a69c604f387e4
PriyaKumari3 <126335227+PriyaKumari3@users.noreply.github.com>
2024-02-23 13:23:08 +0530

    Update program1.js

--- program1.js
+++ program1.js
@@ -3,8 +3,33 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    
+    function  isvalid(s){
+        const stack =[];
+        const pairs={
+            '(':'),
+                '[':'],
+            '{':'),
 };
+        for (let char of s){
+            if(pairs[char]){
+                stack.push(char]);
+            }
+            else{ 
+                const openBracet=stack.pop();
+                if(pairs[openBracket]!==char)
+                {
+                    return false;
+                }
+            }
+        }
+        return stack.length==0;
+    }
+    console.log(isvalid("()"));
+    console.log(isvalid("()[]{}"));
+    console.log(isvalid("(]"));
+    console.log(isvalid("([)"));
+     console.log(isvalid("{[]}"));
+            
 
 module.exports = { isValid };
 
